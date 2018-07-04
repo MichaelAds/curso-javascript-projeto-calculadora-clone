4e5796a1dccb547d7669e14458ffb190a44d77c2
michaelads <maike_maicon09@hotmail.com>
2018-07-03 21:50:44 -0300

    primeiros passos

---
+++ scripts/controller/calcController.js
@@ -0,0 +1,26 @@
+//Estrutura de Orientação a objeto
+// criando a class
+
+class CalcController {
+    constructor(){
+        this._displayCalc = "0";
+        this._currentDate; 
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor
+    }
+
+    get dataAtual(){
+        return this._currentDate
+    }       
+
+    set dataAtual(valor){
+        this._currentDate = valor
+    }
+
+}
